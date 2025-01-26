21b514572877752662a8051b0586ea33b08dc252
LinuxUsersLinuxMint <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2025-01-27 01:14:57 +0300

    Very soon...

---
+++ app_variables.h
@@ -0,0 +1,45 @@
+#ifndef _APP_VARIABLES_H_
+#define _APP_VARIABLES_H_
+
+/*
+Software Number:
+LinuxUsersLinuxMint-Calculator: 01
+LinuxUsersLinuxMint-Calculator-Lite: 02
+Basic_Maths: 03
+AdWMGUI: 04
+PyAppDevKit: 05
+YoutubeDownloader: 06
+CAppDevKit: 07
+CAppDevKit-Insiders: 08
+PyTerm: 09
+LinuxUsersLinuxMint-Calculation-For-Students: 10
+Python-Module: 11
+
+
+Alphabet Equivalent:
+LinuxUsersLinuxMint-Calculator: A
+LinuxUsersLinuxMint-Calculator-Lite: B
+Basic_Maths: C
+AdWMGUI: D  
+PyAppDevKit: E
+YoutubeDownloader: F
+CAppDevKit: G
+CAppDevKit-Insiders: H  
+PyTerm: I
+LinuxUsersLinuxMint-Calculation-For-Students: J
+Python-Module: K
+
+*/
+
+char software_type[];
+char software_number[];
+char alphabet_equivalent[];
+char software_version[];
+char download_file_type[];
+
+char software_download_url[];
+
+char dot[] = ".";
+char two_dots[] = ":";
+
+#endif
